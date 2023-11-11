1eb999682c2868aadf915b0743e1cf8650b386d6
kkstone <10545609+kkstone@users.noreply.github.com>
2023-11-11 18:11:55 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,3 +18,4 @@ wget https://gist.github.com/kkstone/d531dab3fd8b37ba692902ce2c06cde3/raw/da951a
 # Add a feed source
 cd package
 git clone https://github.com/kiddin9/openwrt-packages.git
+rm -rf openwrt-packages/opkg

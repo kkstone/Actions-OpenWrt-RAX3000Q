372538434dc79cf7ea24a8877a763afa52bd1a0a
kkstone <10545609+kkstone@users.noreply.github.com>
2023-11-13 09:04:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,4 +20,4 @@ cd package
 git clone https://github.com/kenzok8/small.git
 git clone https://github.com/kenzok78/luci-app-adguardhome.git
 git clone https://github.com/sundaqiang/openwrt-packages.git
-
+git clone https://github.com/jerrykuku/lua-maxminddb.git

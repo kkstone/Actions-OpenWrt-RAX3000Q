0b2e3eac9897124156c59720af61bdf10e875255
kkstone <10545609+kkstone@users.noreply.github.com>
2024-03-13 18:55:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,3 +21,6 @@ git clone https://github.com/kenzok8/small.git
 git clone https://github.com/kenzok78/luci-app-adguardhome.git
 git clone https://github.com/sundaqiang/openwrt-packages.git
 git clone https://github.com/jerrykuku/lua-maxminddb.git
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang

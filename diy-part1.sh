5acaecf9dfab5fc2738f0a0a65789f3f35466211
kkstone <10545609+kkstone@users.noreply.github.com>
2024-09-06 09:52:38 +0800

    Delete all 3rd feeds

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,13 +12,4 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-rm -rf target/linux/ipq40xx/patches-5.10/999-ipq40xx-unlock-cpu-frequency.patch
-wget https://gist.github.com/kkstone/d531dab3fd8b37ba692902ce2c06cde3/raw/da951a1d138d8ed24aa0c21e28f2b8c7bfd4ace7/993-ipq40xx-unlock-cpu-frequency.patch -O target/linux/ipq40xx/patches-5.10/999-ipq40xx-unlock-cpu-frequency.patch
-
-# Add a feed source
-
-cd package
-git clone https://github.com/kenzok8/small.git
-git clone https://github.com/sundaqiang/openwrt-packages.git
-git clone https://github.com/jerrykuku/lua-maxminddb.git
 
